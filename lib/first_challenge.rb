4abfb5fe9fb26a54d74ec272837accf3ef220af2
Manuela  Sanchez <m.sanchez947@hotmail.com>
2019-01-18 17:28:33 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -19,7 +19,7 @@ contacts.each do |person, data|
   data.each do |attribute, value|
     if attribute == :favorite_icecream_flavors
 .delete_if { |flavor| flavor == "strawberry"}
-
+end
   #remember to return your newly altered contacts hash!
   contacts
 end
